03ed8a29fb7ab2cce957a9a70db2a3a5eb8a7cc9
FAyzulloh12122012 <13091988@gmail.com>
2025-11-17 18:45:25 +0500

    add project

---
+++ transform.js
@@ -0,0 +1,16 @@
+alert("hallo world");
+
+let bosh = confirm("Grid tizimi haqida ma'lumot keragmi?");
+console.log(bosh);
+
+let orta = prompt("Grid tizimini qanchalik bilasiz?");
+console.log(orta);
+
+let oxiri = confirm("Berilgan ma'lumotlarni tasdiqlang.");
+console.log(oxiri);
+
+alert(`
+    ${bosh}
+    ${orta}
+    ${oxiri}
+    `)
